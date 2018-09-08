6598bb4c627cbc8dc6b9d5bac19549e31f3d2f1a
George Zhang <georgezhang@ucla.edu>
2018-09-08 00:50:31 -0700

    delete item mutation to server

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -64,8 +64,20 @@ function addItem(parent, args, context, info) {
   );
 }
 
+function deleteItem(parent, args, context, info) {
+  return context.db.mutation.deleteItem(
+    {
+      where: {
+        id: args.id
+      }
+    },
+    info
+  );
+}
+
 module.exports = {
   signup,
   login,
-  addItem
+  addItem,
+  deleteItem
 };
